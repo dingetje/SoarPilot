deb6dc3b886662690ff8b83838051cfde72b9c37
agmegens <609871+dingetje@users.noreply.github.com>
2014-02-19 22:54:36 +0000

    Can parse IGC now and store in new SoaringPilot_sim database. This will be input for GPS simulator.

--- soarIGC.c
+++ soarIGC.c
@@ -13,72 +13,50 @@
 extern Int32  rxrecs;
 extern char rxtype[15];
 extern Char buf[PARSELEN];
+static Int16  igcidx = 0;
 
-static t_index pnts=0;	/* no of points in track */
-static t_point *pointlist = 0;	/* start point of track list */
-
-// cleanup old list if we have one
-static void cleanUpList()
+/*
+* convert IGC string into numeric latitude or longitude
+*/
+static double getlatlon(char *str)
 {
-	struct _point *pCleanup = 0;
-	while(pointlist)
-	{
-		pCleanup = pointlist;
-		pointlist = pointlist->next;
-		FreeMem((void *) pCleanup);
-	}
-	pnts = 0;
+	double latlon;
+	str[7] = '\0';
+	latlon = StrToDbl(&(str[2]))/(double)60000.0;
+	str[2] = '\0';
+	latlon += StrToDbl(str);
+	return latlon;
 }
 
-// Add a point to the list
-/* static void addPoint(double lat, double lon, double alt, double speed, double dist, int seconds)
-{
-	static t_point *last = 0;
-	t_point *neu = 0;
-	AllocMem((void *)&neu, sizeof(t_point));
-	MemSet(neu, sizeof(t_point), 0);
-	neu->lat = lat;
-	neu->lon = lon;
-	neu->alt = alt;
-	neu->speed = speed;
-	neu->dist = dist;
-	neu->seconds = seconds;
-	neu->next = 0;
-	// 1st time here?
-	if (!pointlist)
-	{
-		// remember start of the points list
-		pointlist = neu;
-	}
-	else
-	{
-		last->next = neu;
-	}
-	last = neu;
-	pnts++;
-} 
- */
 void igc_parser(Char* serinp, UInt32 length, Boolean reset)
 {
 	UInt32 cur = 0;
-	Int16  igcidx = 0;
 	static Int16 next = 0;
 //	Char tempchar[PARSELEN];
 	static Boolean skip = false;
 	static Boolean lineoverflow = false;
+	double	signlat = 1.0,
+			signlon = 1.0;
+	int		hours=0,
+			minutes=0,
+			seconds=0;
 	
+	SimPoint simpoint;
+	MemSet(&simpoint, sizeof(SimPoint), 0);
+
 	if (reset) {
 		HostTraceOutputTL(appErrorClass, " IGC resetting");
 		next = 0;
 		skip = false;
+		igcidx = 0;
 		MemSet(&buf,sizeof(buf),0);
 		rxrecs = 0;
 		StrCopy(rxtype, "Records");
-		cleanUpList();
+		OpenDBEmpty(sim_db);
 		return;
 	}
 
-	StrCopy(rxtype, "Flights");
+	StrCopy(rxtype, "B-Records");
 
 	while (cur < length) {
 		while( (cur < length) && (next < PARSELEN) && (serinp[cur] != '\n')) {
@@ -114,14 +92,43 @@ void igc_parser(Char* serinp, UInt32 length, Boolean reset)
 					lineoverflow = false;
 
 				} else {				
-				//	TODO parse B record			
-					HostTraceOutputTL(appErrorClass, buf);
-					igcidx++;
+					// parse B records
+					//              1111 1 111 11222 2 2 22222 33333
+					// 0 123456 78 90123 4 567 89012 3 4 56789 01234
+					// B HHMMSS DD MMMMM N DDD MMMMM E V PPPPP GGGGG CR LF
+					//
+					// HHMMSS is time stamp
+					// DD MMMMM is latitude and longitude
+					// PPPPP is pressure altitude
+					// GGGGG is GPS altitude
+					if (next < 35) continue;
+					if (buf[15] != '0') continue;
+					if ((buf[14] != 'N') && (buf[14] != 'S')) continue;
+					if ((buf[23] != 'E') && (buf[23] != 'W')) continue;
+					if (buf[14]=='N') signlat = (double)1.0;
+					if (buf[14]=='S') signlat = (double)(-1.0);
+					if (buf[23]=='W') signlon = (double)(-1.0);
+					if (buf[23]=='E') signlon = (double)1.0;
+					buf[14] = '\0'; buf[23] = '\0'; buf[35] = '\0';
+					// use GPS alt
+					simpoint.alt=StrToDbl(&buf[30]); 
+					simpoint.lat = signlat * getlatlon(&buf[7]);
+					simpoint.lon = signlon * getlatlon(&buf[16]);
+					buf[7] = '\0';	seconds = StrAToI(&buf[5]);
+					buf[5] = '\0';	minutes = StrAToI(&buf[3]);
+					buf[3] = '\0';	hours = StrAToI(&buf[1]);
+					simpoint.seconds = seconds + 60*minutes + 3600*hours;
+					HostTraceOutputTL(appErrorClass, "lat=%s",DblToStr(simpoint.lat,2));
+					HostTraceOutputTL(appErrorClass, "lon=%s",DblToStr(simpoint.lon,2));
+					HostTraceOutputTL(appErrorClass, "alt=%s",DblToStr(simpoint.alt,2));
+					
+					rxrecs++;
+					igcidx = OpenDBAddRecord(sim_db, sizeof(SimPoint), &simpoint, false);
 					if (FrmGetActiveFormID() == form_transfer) {
-						// update record counter on transferscreen
+						// update record counter on transfer screen
 						field_set_value(form_transfer_records, DblToStr(igcidx+1,0));
 					}
-//					HostTraceOutputTL(appErrorClass, "polaridx |%hd|", polaridx);
+//					HostTraceOutputTL(appErrorClass, "igcidx |%hd|", igcidx);
 				}
 			}
 			next=0;
